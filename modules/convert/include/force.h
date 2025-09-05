d5ab661f27eaad4d9b5a440f5a54cc657eb10a40
Thibaut Hérault <thibaut.herault@gmail.com>
2025-09-05 10:57:38 +0200

    feat: adding force conversion

---
+++ modules/convert/include/force.h
@@ -0,0 +1,6 @@
+#ifndef FORCE_H
+#define FORCE_H
+
+double force(double, char*, char*);
+
+#endif
---
+++ modules/convert/src/force.c
@@ -0,0 +1,29 @@
+#include "../include/force.h"
+#include "../include/utils.h"
+
+#include <stdio.h>
+
+#define NUM_UNITS 2
+
+const char *unit_names[NUM_UNITS] = {"lbf", "N"};
+
+double conversion_factors[NUM_UNITS][NUM_UNITS] = {
+    {1,                     4.448222000000000},
+    {0.224808923655339,     1},
+};
+
+double force(double force_in, char* unit_in, char* unit_out) {
+    int from_idx = get_unit_index(unit_in, NUM_UNITS, unit_names);
+    int to_idx = get_unit_index(unit_out, NUM_UNITS, unit_names);
+
+    if (from_idx == -1 || to_idx == -1) {
+        printf("Unknown unit\n");
+        return -1;
+    }
+
+    return force_in * conversion_factors[from_idx][to_idx];
+}
+
+
+
+
