afabf476c2322cb6bf2cc4f6ed4e948aa590c154
Thibaut Hérault <thibaut.herault@gmail.com>
2025-09-07 13:12:38 +0200

    refactor: adding function convert to utils

--- modules/convert/include/utils.h
+++ modules/convert/include/utils.h
@@ -2,5 +2,6 @@
 #define UTILS_H
 
 int get_unit_index(const char*, int, const char**);
+double convert(double, const char*, const char*, int num_units, const char**, double[num_units][num_units]);
 
 #endif
--- modules/convert/src/utils.c
+++ modules/convert/src/utils.c
@@ -1,6 +1,7 @@
 #include "../include/utils.h"
 
 #include <string.h>
+#include <stdio.h>
 
 int get_unit_index(const char *unit, int num_units, const char **units) {
     for (int i = 0; i < num_units; i++) {
@@ -9,4 +10,17 @@ int get_unit_index(const char *unit, int num_units, const char **units) {
         }
     }
     return -1; // not found 
+}
+
+
+double convert(double in, const char* unit_in, const char* unit_out, int num_units, const char** unit_names, double conversion_factors[num_units][num_units]) {
+    int from_idx = get_unit_index(unit_in, num_units, unit_names);
+    int to_idx = get_unit_index(unit_out, num_units, unit_names);
+    
+    if (from_idx == -1 || to_idx == -1) {
+        printf("Unknown unit\n");
+        return -1;
+    }
+
+    return in * conversion_factors[from_idx][to_idx];
 }
